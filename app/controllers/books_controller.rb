4c17be2cd32848917cd17c8c312374ac5f0281f0
EC2 Default User <ec2-user@ip-172-31-35-48.ap-northeast-1.compute.internal>
2024-07-24 06:28:35 +0000

    bookers2 -バリテーション-

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,35 +1,72 @@
 class BooksController < ApplicationController
-    def new
-       @book = Book.new
+  before_action :authenticate_user!
+  before_action :is_matching_login_user, only: [:new, :edit, :update, :destroy]
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+    @uer = current_user
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to user_path(current_user)
+    else
+      @books = Book.all
+      @users = User.all
+      @user = current_user
+      render :index
     end
-    
-    def create
-        @book = Book.new(book_params)
-        @book.user_id = current_user.id
-        @book.save
-        redirect_to user_path(id: current_user)
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @users = User.all
+    @user = current_user
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @books = Book.all 
+  end
+
+  def edit
+    is_matching_login_user
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book updated successfully."
+      redirect_to book_path(@book)
+    else
+      render :edit
     end
-    
-    def index
-        @books = Book.all
-        @book = Book.new
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    flash[:notice] = "Book deleted successfully."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def is_matching_login_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+      flash[:alert] = "You are not authorized to perform this action."
+      redirect_to books_path
     end
-    def create
-        book = Book.new(book_params)
-        flash[:notice] = "successfully" if book.save
-        redirect_to book_path(book)
-    end
-    def show
-        @book = Book.find(params[:id])
-    end
-    def destroy
-        book = Book.find(params[:id])
-        book.delete
-        redirect_to books_path
-    end
-    private
-    
-    def book_params
-      params.require(:book).permit(:title, :body, :image).merge(user_id: current_user.id)
-    end
+  end
 end
